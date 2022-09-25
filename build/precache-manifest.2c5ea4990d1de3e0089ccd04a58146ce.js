28036d9369043f8d1774a89cad5ded356f604b5f
francho <franchuf@gmail.com>
2022-09-25 09:12:24 -0300

    dale2

---
+++ build/precache-manifest.2c5ea4990d1de3e0089ccd04a58146ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30a3176e59613057d8163dfffd42509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "a72cd5e64f3a1090de4b",
+    "url": "/static/js/main.e33d0725.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
